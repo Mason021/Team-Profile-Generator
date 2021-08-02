f51f6f43628fc37bf786d544287ae442778dfa50
Mason021 <mdc5054@gmail.com>
2021-08-01 22:59:01 -0500

    updated with video and sample generated html

--- src/page-template.js
+++ src/page-template.js
@@ -3,8 +3,8 @@ const createCards = employeeGroupData => {
     const manager = employeeGroupData.manager.map(function(work) {
         let managersHtml = `
         <div class="card" style="width: 18rem;">
-            <h2>${work.name}</h2>
-            <h4>Manager<h4>
+            <h2 class="card-title">${work.name}</h2>
+            <h4 class="card-text">Manager<h4>
             <p>ID: ${work.id}</p>
             <p>Email: <a href="mailto:${work.email}">${work.email}</a></p>
             <p>Office Number: ${work.office}</p>
@@ -16,8 +16,8 @@ const createCards = employeeGroupData => {
     const engineer = employeeGroupData.engineer.map(function(work) {
         let engineersHtml = `
         <div class="card" style="width: 18rem;">
-            <h2>${work.name}</h2>
-            <h4>Engineer<h4>
+            <h2 class="card-title">${work.name}</h2>
+            <h4 class="card-text">Engineer<h4>
             <p>ID: ${work.id}</p>
             <p>Email: <a href="mailto:${work.email}">${work.email}</a></p>
             <p> Github: <a href="https://github.com/${work.github}" target="_blank">${work.github}</a></p>
@@ -29,8 +29,8 @@ const createCards = employeeGroupData => {
     const intern = employeeGroupData.intern.map(function(work) {
         let internsHtml = `
         <div class="card" style="width: 18rem;">
-            <h2>${work.name}</h2>
-            <h4>Intern<h4>
+            <h2 class="card-title">${work.name}</h2>
+            <h4 class="card-text">Intern<h4>
             <p>ID: ${work.id}</p>
             <p>Email: <a href="mailto:${work.email}">${work.email}</a></p>
             <p> School: ${work.school}</p>
@@ -49,11 +49,11 @@ module.exports = layoutData => {
             <meta charset="UTF-8">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
             <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-            <title>My Team</title>
+            <title>Employee Database</title>
         </head>
         <body>
             <header>
-            <h1 class="text-center">My Team</h1>
+            <h1 class="text-center">Employee Database</h1>
             </header>
             ${createCards(layoutData)}
         </body>
